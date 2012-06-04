eed836316eee1b8a3b5f37704928f0b95a726b7f
Juca <juca1412@gmail.com>
2012-06-04 14:13:44 -0300

    20120604

--- chrome/content/urlbar.js
+++ chrome/content/urlbar.js
@@ -1,5 +1,5 @@
 function Login(form) {
 var urlbar = form.urlbar.value;
 if (urlbar) {
-var site = "http://" + urlbar; document.getElementById('browser-content').loadURI(site);}
+var site = urlbar; document.getElementById('browser-content').loadURI(site);}
 }
