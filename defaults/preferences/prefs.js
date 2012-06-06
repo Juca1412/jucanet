7d6ac7fc8dd2896ba8a71045970b8dbbd6f4aedb
Juca <juca1412@gmail.com>
2012-06-06 18:35:11 -0300

    20120606

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -2,24 +2,24 @@ pref("toolkit.defaultChromeURI", "chrome://jucanet/content/main.xul");
 
 pref("browser.chromeURL", "chrome://jucanet/content/popups.xul"); // - permite popups
 
-pref(“browser.cache.disk.enable”, false);
+pref("browser.cache.disk.enable", false);
 
-pref(“network.http.pipelining”, true);
+pref("network.http.pipelining", true);
 
-pref(“network.http.proxy.pipelinin”, true);
+pref("network.http.proxy.pipelinin", true);
 
-pref(“content.interrupt.parsing”, true);
+pref("content.interrupt.parsing", true);
 
-pref(“content.max.tokenizing.time”, 2250000);
+pref("content.max.tokenizing.time", 2250000);
 
-pref(“network.http.pipelining.maxrequests”, 8);
+pref("network.http.pipelining.maxrequests", 8);
 
-pref(“content.notify.interval”, 750000);
+pref("content.notify.interval", 750000);
 
-pref(“content.notify.ontimer”, true);
+pref("content.notify.ontimer", true);
 
-pref(“content.notify.backoffcoun”, 5);
+pref("content.notify.backoffcoun", 5);
 
-pref(“content.switch.threshold”, 750000);
+pref("content.switch.threshold", 750000);
 
-pref(“nglayout.initialpaint.delay”, 0);
+pref("nglayout.initialpaint.delay", 0);
